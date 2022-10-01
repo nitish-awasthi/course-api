aa6a52981dbc0ec6784ecd20a54d4b84df505855
Zenix27 <agni.utk27@gmail.com>
2022-10-01 11:27:39 +0530

    implemented getAllTopics, to view with

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,9 +1,21 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
-
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
 }
